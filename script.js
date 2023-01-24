a4eac98fa294247ac29bd2511ff34ecf0257fd82
Zayon Nieves <zayon.nieves1@gmail.com>
2023-01-23 20:16:11 -0500

    added function for sending request to API for data and display to user

--- script.js
+++ script.js
@@ -3,11 +3,44 @@
 let submitBtn = document.querySelector('button');
 let inputBox = document.querySelector('input');
 
+// API information
+let url = 'https://api.nasa.gov/planetary/apod?api_key=';
+let apiKey = process.env.myAPI-key;
+
+
+// display data response from API to user
+function displayData(data) {
+
+    let picture = document.querySelector('img');
+    let description = document.querySelector('p');
+
+    // show image and description from returned data
+    picture.src = data.url;
+    description.innerText = data.explanation;
+
+
+}
+
 
 // eventHandler for click events on Submit button
-function logInput() {
+async function logInput() {
 
-    console.log(inputBox.value);
+    try {
+        // send request to API for data
+        const response = await fetch(url + apiKey);
+
+        if(response.ok) {
+
+            const jsonResponse = await response.json();
+
+            // display data to user
+            displayData(jsonResponse);
+        }
+        throw new Error('Request failed!');
+    }
+    catch (error) {
+        console.log(error);
+    }
 
 }
 
